319418a994a04b79f56564871f5749c6d0689421
gal salomon <gal.salomon@gmail.com>
2020-11-10 17:41:31 +0200

    add initial support for multiple chunks parquet file input. using schema-read instead of ReadTable.

--- include/s3select_oper.h
+++ include/s3select_oper.h
@@ -1477,7 +1477,7 @@ public:
     }
     else if(m_var_type == var_t::STAR_OPERATION)
     {
-      return star_operation();//TODO parquet star operation
+      return star_operation();
     }
     else if (column_pos == undefined_column_pos)
     {
