1dcd260362f841b74781cac887222587b5c7d8a9
galsalomon66 <gal.salomon@gmail.com>
2023-06-16 16:53:11 +0300

    fix syntax issues in TPCDS queries(trino complaints)
    Signed-off-by: galsalomon66 <gal.salomon@gmail.com>

--- TPCDS/sample-queries-tpcds/query92.sql
+++ TPCDS/sample-queries-tpcds/query92.sql
@@ -1,6 +1,6 @@
 -- start query 1 in stream 0 using template query92.tpl and seed 2031708268
 select  
-   sum(ws_ext_discount_amt)  as `Excess Discount Amount` 
+   sum(ws_ext_discount_amt)  as "Excess Discount Amount" 
 from 
     web_sales 
    ,item 
@@ -8,8 +8,8 @@ from
 where
 i_manufact_id = 356
 and i_item_sk = ws_item_sk 
-and d_date between '2001-03-12' and 
-        (cast('2001-03-12' as date) + 90 days)
+and d_date between cast('2001-03-12' as date) and 
+        (cast('2001-03-12' as date) + interval '90' day)
 and d_date_sk = ws_sold_date_sk 
 and ws_ext_discount_amt  
      > ( 
@@ -20,8 +20,8 @@ and ws_ext_discount_amt
            ,date_dim
          WHERE 
               ws_item_sk = i_item_sk 
-          and d_date between '2001-03-12' and
-                             (cast('2001-03-12' as date) + 90 days)
+          and d_date between cast('2001-03-12' as date) and
+                             (cast('2001-03-12' as date) + interval '90' day)
           and d_date_sk = ws_sold_date_sk 
       ) 
 order by sum(ws_ext_discount_amt)
